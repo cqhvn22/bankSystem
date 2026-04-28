0dd5dec9b6b02b538a9765a6416d2bdec7fee4a5
cqhvn22 <chuquanghung200@gmail.com>
2026-04-28 10:26:29 +0700

    Update

--- fix_pass.sql
+++
@@ -1,1 +0,0 @@
-UPDATE mbbank_db.users SET password = '$2a$10$JDrIm7iJJTvHdObNC6.lJOjsp3Ys1bfTj3t4oXvOoGY77jZQkLV4e' WHERE username IN ('admin', '0359217462');
--- fix_passwords.sql
+++
@@ -1,9 +0,0 @@
--- BCrypt hash of "123456" = $2a$10$JDrIm7iJJTvHdObNC6.lJOjsp3Ys1bfTj3t4oXvOoGY77jZQkLV4e (length 60)
--- Fix all users with wrong password length (120 chars = duplicated hash)
-
-UPDATE mbbank_db.users
-SET password = '$2a$10$JDrIm7iJJTvHdObNC6.lJOjsp3Ys1bfTj3t4oXvOoGY77jZQkLV4e'
-WHERE LENGTH(password) != 60;
-
--- Verify
-SELECT id, username, role, LENGTH(password) as len FROM mbbank_db.users;
