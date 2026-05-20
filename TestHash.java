bec1f19e9369bbf9a4d2ca17b7676eb12248e175
cqhvn22 <chuquanghung200@gmail.com>
2026-05-20 15:31:59 +0700

    Delete TestHash.java

--- TestHash.java
+++
@@ -1,9 +0,0 @@
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-public class TestHash {
-    public static void main(String[] args) {
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        System.out.println(encoder.matches("123456", "$2a$10$S2hcjCFJjRQaHF7Bw/XN3esUkhEJthUtVRirYvV3e0iBnt3JemJeu"));
-        System.out.println(encoder.encode("123456"));
-    }
-}
