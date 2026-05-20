e1897e35a822266e4aae7e21061f7947914d05f9
cqhvn22 <chuquanghung200@gmail.com>
2026-05-20 15:33:25 +0700

    Delete fix_encoding.ps1

--- fix_encoding.ps1
+++
@@ -1,8 +0,0 @@
-$files = Get-ChildItem -Path "C:\Users\ADMIN\Downloads\mbbSystem\src\main\resources\static" -Recurse -Filter *.html
-foreach ($f in $files) {
-    $content = Get-Content $f.FullName -Raw -Encoding UTF8
-    if ($content -notmatch '<meta charset="UTF-8">') {
-        $content = $content -replace '<head>', "<head>`n    <meta charset=`"UTF-8`">"
-        Set-Content -Path $f.FullName -Value $content -Encoding UTF8
-    }
-}
